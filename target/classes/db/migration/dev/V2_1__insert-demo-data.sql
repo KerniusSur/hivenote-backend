3f996f8bc532c6ff40eab6478185a12ac16cf069
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2024-04-10 22:20:14 +0300

    updated gitignore

--- target/classes/db/migration/dev/V2_1__insert-demo-data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO role(id, name)
-VALUES (gen_random_uuid(), 'USER'),
-       (gen_random_uuid(), 'ADMIN');
-
-INSERT INTO account(name, last_name, email, password, phone_number)
-VALUES ('Kernius', 'Sur', 'kernius.survila@gmail.com', '$2a$10$XgXwTP4l7N43Q64BGA8gve/ZqadAGCNTHBTUrih3nGpBf4WRy3hay',
-        '123456789');
-
--- Pass: password123
