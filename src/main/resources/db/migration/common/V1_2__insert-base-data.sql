2f3e6832955c11fe3c619f5f23eb5e55019be724
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2024-04-11 12:12:04 +0300

    add roles to common migration

---
+++ src/main/resources/db/migration/common/V1_2__insert-base-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO role(id, name)
+VALUES (gen_random_uuid(), 'USER'),
+       (gen_random_uuid(), 'ADMIN');
--- src/main/resources/db/migration/dev/V2_1__insert-demo-data.sql
+++ src/main/resources/db/migration/dev/V2_1__insert-demo-data.sql
@@ -1,7 +1,3 @@
-INSERT INTO role(id, name)
-VALUES (gen_random_uuid(), 'USER'),
-       (gen_random_uuid(), 'ADMIN');
-
 INSERT INTO account(name, last_name, email, password, phone_number)
 VALUES ('Kernius', 'Sur', 'kernius.survila@gmail.com', '$2a$10$XgXwTP4l7N43Q64BGA8gve/ZqadAGCNTHBTUrih3nGpBf4WRy3hay',
         '123456789');
